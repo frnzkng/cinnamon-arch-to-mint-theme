5af210dc4e9f915093a29c2ff1cefce59ced70da
frnzkng <136699979+frnzkng@users.noreply.github.com>
2023-09-14 15:41:25 +0200

    Create InstallCinnamon+LangPack.sh

---
+++ InstallCinnamon+LangPack.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Script that installs Cinnamon + language pack"
+# Install cinnamon DE
+sudo pacman -S cinnamon cinnamon-translations
+echo "Installation of Cinnamon and the language pack complete."
