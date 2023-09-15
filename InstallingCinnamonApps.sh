68921c1094bd033f546a840bff62f8f077a6fb4e
frnzkng <136699979+frnzkng@users.noreply.github.com>
2023-09-15 14:15:52 +0200

    Update InstallingCinnamonApps.sh

--- InstallingCinnamonApps.sh
+++ InstallingCinnamonApps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "This script will install the apps for Cinnamon. You must have yay and cinnamon installed."
-sleep (10)
-echo "Okay, looks like you want to continue. Installing the pacman packages first."
 # installing the packages from the pacman repo
 sudo pacman -S xed xreader
 echo "Finished the installation of the first few packages. Now continuing with the AUR packages"
