089787328595aaafed7ca31cc8cbe7b141ebfd03
frnzkng <136699979+frnzkng@users.noreply.github.com>
2023-09-15 14:09:49 +0200

    Update yayInstaller.sh

--- yayInstaller.sh
+++ yayInstaller.sh
@@ -5,4 +5,4 @@ sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-echo "Installation of git and yay complete."
+echo "Installation of yay complete."
